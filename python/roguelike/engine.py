b40cb8d7c7f308dbdff6ace1fb77f01894f4b24b
Charles Laforte <charles.takeru.laforte@gmail.com>
2019-06-14 20:16:02 -0400

    Finished tutorial 3 of creating a roguelike game

--- python/roguelike/engine.py
+++ python/roguelike/engine.py
@@ -13,6 +13,10 @@ def main():
     map_width = 80
     map_height = 45
 
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
         'dark_ground': libtcod.Color(50, 50, 150)
@@ -24,12 +28,12 @@ def main():
 
     libtcod.console_set_custom_font('roguelike/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(screen_width, screen_height, 'Detekoi, boku no tomodachi, jibanyan, pretty pretty i am lovely pretty, pretty pretty pretty, Jibanyan', False)
+    libtcod.console_init_root(screen_width, screen_height, 'ibanyan', False)
 
     con = libtcod.console_new(screen_width, screen_height)
 
     game_map = GameMap(map_width, map_height)
-    game_map.make_map()
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
