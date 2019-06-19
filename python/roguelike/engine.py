b2f47d1c01c8b80e0c89543451341871e7f00ed5
Charles Laforte <charles.takeru.laforte@gmail.com>
2019-06-18 20:29:35 -0400

    infinite life was stolen by my father

--- python/roguelike/engine.py
+++ python/roguelike/engine.py
@@ -1,6 +1,7 @@
 import tcod as libtcod
 
 from .components.fighter import Fighter
+from .death_functions import kill_monster, kill_player
 from .entity import Entity, get_blocking_entities_at_location
 from .fov_functions import initialize_fov, recompute_fov
 from .game_states import GameStates
@@ -74,6 +75,8 @@ def main():
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
+        player_turn_results = []
+
         if move and game_state == GameStates.PLAYERS_TURN:
             dx, dy = move
 
@@ -84,7 +87,8 @@ def main():
                 target = get_blocking_entities_at_location(entities, destination_x, destination_y)
 
                 if target:
-                    player.fighter.attack(target)
+                    attack_results = player.fighter.attack(target)
+                    player_turn_results.extend(attack_results)
                 else:
                     player.move(dx, dy)
 
@@ -98,11 +102,49 @@ def main():
         if fullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
+        for player_turn_results in player_turn_results:
+            message = player_turn_results.get('message')
+            dead_entity = player_turn_results.get('dead')
+
+            if message:
+                print(message)
+
+            if dead_entity:
+                # We'll do something here momentarily
+                if dead_entity == player:
+                    message, game_state = kill_player(dead_entity)
+                else:
+                    message = kill_monster(dead_entity)
+
+                print(message)                
+
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
                 if entity.ai:
-                    entity.ai.take_turn(player, fov_map, game_map, entities)
-                
+                    enemy_turn_results = entity.ai.take_turn(player, fov_map, game_map, entities)
+
+                    for enemy_turn_result in enemy_turn_results:
+                        message = enemy_turn_result.get('message')
+                        dead_entity = enemy_turn_result.get('dead')
+
+                        if message:
+                            print(message)
+
+                        if dead_entity:
+                            if dead_entity == player:
+                                message, game_state = kill_player(dead_entity)
+                            else:
+                                message = kill_monster(dead_entity)
+
+                            print(message)
+
+                            if game_state == GameStates.PLAYER_DEAD:
+                                break
+
+                    if game_state == GameStates.PLAYER_DEAD:
+                        break
+
+            else:                
                 game_state = GameStates.PLAYERS_TURN
 
 if __name__ == '__main__':
